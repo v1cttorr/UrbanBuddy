152903bbefe38b35a333ce49f6af581ed923b36a
VP859 <mierzwa.jan.6@gmail.com>
2025-03-14 21:17:48 +0100

    chat history

--- gpt/forms.py
+++ gpt/forms.py
@@ -4,9 +4,9 @@ from crispy_forms.layout import Submit
 
 class ChatBotPromptForm(forms.Form):
     prompt = forms.CharField(widget=forms.Textarea(attrs={'rows': 1, 'cols': 50}))
-    
+
     def __init__(self, *args, **kwargs):
         super(ChatBotPromptForm, self).__init__(*args, **kwargs)
         self.helper = FormHelper()
         self.helper.form_method = 'post'
-        self.helper.add_input(Submit('submit', 'Generate Text'))
+        self.helper.add_input(Submit('submit', 'Submit'))
